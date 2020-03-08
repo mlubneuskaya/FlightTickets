2588551f9a3234a59cd33f0e61453c0422fb5966
mlubneuskaya <m.lubnewskaja@gmail.com>
2020-03-08 20:05:26 +0300

    warnings

--- src/com/fligths/server/ServerFlightCommandExecutor.java
+++ src/com/fligths/server/ServerFlightCommandExecutor.java
@@ -15,8 +15,6 @@ class ServerFlightCommandExecutor {
     List<String> executeCommand(FlightCommand command){
         switch(command.getName().get(0)) {
             case "findTicket":
-                System.out.println("find ticket");
-                System.out.println(command.getParams().get(0) + command.getParams().get(1));
                 return flightTicket.findTicket(command.getParams().get(0), command.getParams().get(1));
             case "findCheapestTicket":
                 return flightTicket.findCheapest(command.getParams().get(0));
